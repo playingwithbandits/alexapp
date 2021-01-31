07deaab0af1a65c74b7962441ad2cfcfabe95924
Alex Dawson <alexdawson234@hotmail.co.uk>
2021-01-31 23:21:51 +0000

    button

--- src/Helpers/HelperFunc.tsx
+++ src/Helpers/HelperFunc.tsx
@@ -1,9 +1,31 @@
 export const GET_CURRENT_DATE = () => {
-    return "2021-01-21"
+    let today = new Date();
+    let todayStr = today.toISOString().split('T')[0];
+    return todayStr;
 }
 export const GET_DIFF_DAYS_STRS = (d1:string, d2:string) => {
     const date1 = new Date(d1).getTime();
     const date2 = new Date(d2).getTime();
     const diffTime = Math.abs(date2 - date1);
     return Math.ceil(diffTime / (1000 * 60 * 60 * 24)); 
+}
+
+export const GET_DAYS_BETWEEN_INC = (d1:string, d2:string) => {
+    let startDateInclucive = d1;
+    let endDateInclucive = d2;
+    let listOfDates = [];
+    let stopDateLoop = false;
+    for(let i = 0; i < 10000; i++){
+        let date = new Date(startDateInclucive);
+        date.setDate(date.getDate() - i);
+        let tempEndDateString = date.toISOString().split('T')[0];
+        
+        if(!stopDateLoop){
+            listOfDates.push(tempEndDateString);
+        }
+        if(tempEndDateString == endDateInclucive){
+            stopDateLoop = true;
+        }
+    }
+    return listOfDates; 
 }
--- src/components/Eyecatchers/Eyecatchers.tsx
+++ src/components/Eyecatchers/Eyecatchers.tsx
@@ -1,8 +1,8 @@
-import { useMutation, useQuery } from "@apollo/client";
+import { FetchResult, MutationFunctionOptions, MutationResult, useMutation, useQuery } from "@apollo/client";
 import { useEffect } from "@storybook/addons";
 import React, { useState } from "react";
 import { Box, Button, Flex, Text } from "rebass";
-import { GET_CURRENT_DATE, GET_DIFF_DAYS_STRS } from "../../Helpers/HelperFunc";
+import { GET_CURRENT_DATE, GET_DIFF_DAYS_STRS, GET_DAYS_BETWEEN_INC } from "../../Helpers/HelperFunc";
 import { M_Q } from "../../queries/myQueries";
 import { Loading } from "../Loading";
 import { Table } from "../Table";
@@ -15,45 +15,52 @@ interface Eyecatcher{
   note: string;
 }
 
-const addEyecatcher = () => {
+const addEyecatcher = (date: string, data: Eyecatcher[], insertEyecatcher: (options?: MutationFunctionOptions<any, Record<string, any>> | undefined) => Promise<FetchResult<any, Record<string, any>, Record<string, any>>>, returning: MutationResult<any>) => {
 
+
+  const ecObj: Eyecatcher = { created: date, name: "test", note: "test"};
+  insertEyecatcher({ variables: ecObj });
+
+  console.log(returning);
+
+  //data?.push(ecObj);
 };
 
+const columnsForTable = [
+  {
+    name: "Name",
+    selector: "name",
+    sortable: true
+  },
+  {
+    name: "Note",
+    selector: "note",
+    sortable: true
+  },
+  {
+    name: "Created",
+    selector: "created",
+    sortable: true,
+    right: true
+  }
+];
 
 export const Eyecatchers: React.FC<EyecatchersProps> = (props) => {
   
   const { data, loading, error } = useQuery(M_Q.GET_ALL_EYECATCHERS, {});
-  const lastUpdate = data?.Eyecatchers_aggregate?.aggregate?.max?.created;
+  const [insertEyecatcher, returning] = useMutation(M_Q.INSERT_EYECATCHER);
+
+  const [lastUpdate, setLastUpdate] = useState(data?.Eyecatchers_aggregate?.aggregate?.max?.created);
   const currentDate = GET_CURRENT_DATE();
-  const diff_days = GET_DIFF_DAYS_STRS(lastUpdate, currentDate);
+  const listOfDates = currentDate && lastUpdate ? GET_DAYS_BETWEEN_INC(currentDate, lastUpdate) : [];
+  const diff_days = currentDate && lastUpdate ? GET_DIFF_DAYS_STRS(currentDate, lastUpdate) : 0;
 
   const [eyecatchers, setEyecatchers] = useState(undefined);
-  const [addTodo, returning] = useMutation(M_Q.INSERT_EYECATCHER);
-
-  const columns = [
-    {
-      name: "Name",
-      selector: "name",
-      sortable: true
-    },
-    {
-      name: "Note",
-      selector: "note",
-      sortable: true
-    },
-    {
-      name: "Created",
-      selector: "created",
-      sortable: true,
-      right: true
-    }
-  ];
-
 
   const onClick = () => {
-    console.log(currentDate);
-    addTodo({ variables: { created: currentDate, name: "test", note: "test"} });
-    console.log(returning);
+    listOfDates.map(date => addEyecatcher(date, data?.Eyecatchers, insertEyecatcher, returning))
+
+    setLastUpdate(currentDate);
   }
 
   return <>
@@ -65,13 +72,13 @@ export const Eyecatchers: React.FC<EyecatchersProps> = (props) => {
               {lastUpdate && 
                 <Box>
                   <Text>Last records: {lastUpdate} ({diff_days} days)</Text>
-                  {lastUpdate !== currentDate && <Button onClick={onClick}>Update</Button>}
+                  {lastUpdate !== currentDate && <Button onClick={onClick} >Update</Button>}
                   <Text>{returning.loading}</Text>
                 </Box>
               }
               
             </Box>
-            <Table data={data.Eyecatchers} columns={columns} size="small" defaultSortField="created" pagination selectableRows stickyHeader></Table>
+            <Table data={data.Eyecatchers} columns={columnsForTable} defaultSortField="created" pagination selectableRows stickyHeader></Table>
             
           </Flex>
           
